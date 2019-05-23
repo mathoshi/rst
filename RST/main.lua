a8f7a7ffec24d1c09ee867d1fd7ebdae6994bfd0
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-05-23 13:30:06 -0400

    Update main.lua

--- RST/main.lua
+++ RST/main.lua
@@ -10,14 +10,27 @@ local title = display.newText( "TypeLearner", display.contentCenterX, 30, "Times
 
 local keyboard = display.newImageRect( "assets/keyboard.png", 300, 111)
 keyboard.x = display.contentCenterX
-keyboard.y = 150
+keyboard.y = 160
 
+local nameBox = native.newTextField(display.contentCenterX, 80, 200, 15)
 
-local function keys( event )
-	--if (event.keyName == "a" and event.phase == "down") then
-		print("Key '".. event.keyName .. "' was pressed " .. event.phase)
-		return true
-	--end
+local nameText = display.newText( "", display.contentCenterX, 80, "Times", 15)
+
+--local function keys( event )
+--	--if (event.keyName == "a" and event.phase == "down") then
+--		print("Key '".. event.keyName .. "' was pressed " .. event.phase)
+--		return true
+--	--end
+--end
+
+local function namePrint( event )
+	if (nameBox.text ~= nil) then
+		if ((event.keyName == "enter" or event.keyName == "numPadEnter") and (event.phase == "down")) then
+			nameText.text = "Hi, ".. nameBox.text ..". Welcome!"
+			display.remove( nameBox )
+		end
+	end
 end
 
-Runtime:addEventListener( "key", keys)
+ADD EVNET LISTIWEEBR FOR namePrint
+--Runtime:addEventListener( "key", keys)
