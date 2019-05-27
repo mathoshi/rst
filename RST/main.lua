4ee0dad2693b6b7495d065b07b873ea7d7a5811d
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-05-27 13:30:04 -0400

    Update main.lua

--- RST/main.lua
+++ RST/main.lua
@@ -6,15 +6,25 @@
 -- This file contains TypeLearner, a game to pratice your keyboard typing.
 -----------------------------------------------------------------------------------------
 
+local background = display.setDefault( "background", 100/255, 100/255, 100/255)
+
 local title = display.newText( "TypeLearner", display.contentCenterX, 30, "Times", 20)
 
 local keyboard = display.newImageRect( "assets/keyboard.png", 300, 111)
 keyboard.x = display.contentCenterX
-keyboard.y = 160
+keyboard.y = 240
 
 local nameBox = native.newTextField(display.contentCenterX, 80, 200, 15)
 
-local nameText = display.newText( "", display.contentCenterX, 80, "Times", 15)
+local nameText = display.newText( "", display.contentCenterX, 100, "Times", 10)
+
+local enterButon = display.newImageRect( "assets/button.png", 30, 30)
+enterButon.x = 370
+enterButon.y = 80
+
+local startButton = display.newImageRect( "assets/startButton.png", 60, 47)
+startButton.x = display.contentCenterX
+startButton.y = 160
 
 --local function keys( event )
 --	if (event.keyName == "a" or event.keyName == "j") and (event.phase == "down") then
@@ -24,19 +34,25 @@ local nameText = display.newText( "", display.contentCenterX, 80, "Times", 15)
 --end
 
 local function namePrint( event )
-	if ((event.keyName == "enter" or event.keyName == "numPadEnter") and (event.phase == "down")) then
+	if (nameBox.text ~= "") then
+		nameText.text = "Hi, ".. nameBox.text ..". Welcome!"
+		print("Hi")
+	else
+		print("write yuor name")
 		nameText.text = "Write your name."
-		if (nameBox.text ~= "") then
-			nameText.text = "Hi, ".. nameBox.text ..". Welcome!"
-			print("Hi")
-			print(event.keyName)
-			
-		else
-			print("write yuor name")
-		end
 	end
 end
 			
-
-Runtime:addEventListener( "key", namePrint)
+enterButon:addEventListener( "touch", namePrint)
 --Runtime:addEventListener( "key", keys)
+
+local function start( event )
+	display.remove(startButton)
+	display.remove(enterButon)
+	display.remove(nameBox)
+	keyboard.y = 130
+
+end
+
+startButton:addEventListener( "touch", start)
+
